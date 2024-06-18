3efd0bc1ec8332a42f2da4971648434bd6c450e2
Arash Nemat Zadeh <Arashnm80@gmail.com>
2024-06-18 17:56:37 +0000

    edit

--- variables.py
+++ variables.py
@@ -1,11 +1,11 @@
 import os
 
 # twitter tokens (consumer key is the api key)
-# TWITTER_BEARER_TOKEN = os.environ["TWITTER_BEARER_TOKEN"]
-# TWITTER_CONSUMER_KEY = os.environ["TWITTER_CONSUMER_KEY"]
-# TWITTER_CONSUMER_SECRET =os.environ["TWITTER_CONSUMER_SECRET"]
-# TWITTER_ACCESS_TOKEN = os.environ["TWITTER_ACCESS_TOKEN"]
-# TWITTER_ACCESS_TOKEN_SECRET = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+TWITTER_BEARER_TOKEN = os.environ["TWITTER_BEARER_TOKEN"]
+TWITTER_CONSUMER_KEY = os.environ["TWITTER_CONSUMER_KEY"]
+TWITTER_CONSUMER_SECRET =os.environ["TWITTER_CONSUMER_SECRET"]
+TWITTER_ACCESS_TOKEN = os.environ["TWITTER_ACCESS_TOKEN"]
+TWITTER_ACCESS_TOKEN_SECRET = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
 
 # settings
 quotes_page_link = "https://raw.githubusercontent.com/arashnm80/arashnm80.github.io/main/handbook/quotes.md"
