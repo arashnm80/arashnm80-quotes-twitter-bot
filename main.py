2a3b9db702d8eab71fb1da03f21eb0f936f44c6c
Arash Nemat Zadeh <arashnm80@gmail.com>
2024-06-30 04:45:20 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from functions import *
 if __name__ == "__main__":
     quotes_list = get_quotes_list()
     tweet_text = random.choice(quotes_list)
+    tweet_text = bold_text(tweet_text)
     # tweet_text = today_date() + "\n\n" + tweet_text
     if len(tweet_text) > 280:
         print("tweet text more than 280 characters")
